1f7445cabde7c60e0c614fb697d0efea4856e683
21700185-kimtaehong <bighong12@gmail.com>
2022-11-08 17:20:25 +0900

    Initial commit

---
+++ src/main/java/com/example/util/JDBCUtil.java
@@ -0,0 +1,25 @@
+package com.example.jspfirst_proejct.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil {
+	public static Connection getConnection(){  
+	    Connection con=null;  
+	    try{  
+	        Class.forName("com.mysql.jdbc.Driver");  
+	        con= DriverManager.getConnection("jdbc:mysql://db4free.net:3306/dbname","p_21700185","eeghoC8dai");
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }  
+	    return con;  
+	}  
+	
+	public static void main(String ars[]) {
+		Connection conn = getConnection();
+		if(conn != null)
+			System.out.println("DB 연결됨!");
+		else
+			System.out.println("DB 연결중 오류 !");
+	}
+}
